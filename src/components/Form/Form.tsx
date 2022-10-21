51ccb97ce27b99fabb92d561478601cc5ad20ce3
hennifant <henning.schwichow@gmx.de>
2022-10-21 17:07:37 +0200

    Fixed recaptcha issue

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -76,7 +76,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6LcyjpwiAAAAAMQhFfs-TxFERxwiV4PDi10f-Qny"
+          sitekey="6LcyjpwiAAAAAHdIX1PjZovJte8V2kCBwPFQREeA"
           onChange={(e) => {
             setIsHuman(true);
           }}
